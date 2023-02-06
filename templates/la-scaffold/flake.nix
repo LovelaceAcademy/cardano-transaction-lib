6bafaad3a1596473c2617c7f426806b9d231cfdc
Walker <walker@praiseweb.com.br>
2023-02-06 16:22:03 -0300

    remove ctl cache

--- templates/la-scaffold/flake.nix
+++ templates/la-scaffold/flake.nix
@@ -104,12 +104,10 @@
     extra-substituters = [
       "https://cache.nixos.org"
       "https://cache.iog.io"
-      "https://cardano-transaction-lib.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "cardano-transaction-lib.cachix.org-1:Fbil3zlpdx+NxCubSPmmrWw03LvklGaZf1T04l1H3vo="
     ];
     allow-import-from-derivation = "true";
   };
